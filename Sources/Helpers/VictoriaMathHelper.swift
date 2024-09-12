f80715ab37f118a12094a52f7fc8b7b8f0889867
Rokas Sidaravicius <rokas.sidaravicius@presentconnection.eu>
2024-09-12 11:27:24 +0300

    refactor: Renamed module folder and swift file

---
+++ Sources/Helpers/VictoriaMathHelper.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct VictoriaMathHelper {
+    public static func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
