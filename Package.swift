04ebaf4d75b4f4d424dce6e15060a15d9cb98157
Boon <boon@big-endian.nl>
2024-10-18 15:29:48 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "victoria_id_sdk_nfc_ios",
+    name: "victoria_id_check_id_sdk_ios",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "victoria_id_sdk_nfc_ios",
-            targets: ["victoria_id_sdk_nfc_ios"]),
+            name: "victoria_id_check_id_sdk_ios",
+            targets: ["victoria_id_check_id_sdk_ios"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "victoria_id_sdk_nfc_ios"),
+            name: "victoria_id_check_id_sdk_ios"),
         .testTarget(
-            name: "victoria_id_sdk_nfc_iosTests",
-            dependencies: ["victoria_id_sdk_nfc_ios"]),
+            name: "victoria_id_check_id_sdk_iosTests",
+            dependencies: ["victoria_id_check_id_sdk_ios"]),
     ]
 )
