5b24b9b452d556cdbb002c1c81902a28a4d45250
Rokas Sidaravicius <rokas.sidaravicius@presentconnection.eu>
2024-11-18 01:13:51 +0200

    temp: SDK update

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "victoria_id_check_id_sdk_ios",
-            targets: ["SimpleSomething"]),
+            targets: ["victoria_id_check_id_sdk_ios"]),
     ],
     targets: [
         .binaryTarget(
-            name: "SimpleSomething",
-            url: "https://github.com/victoria-id/victoria_id_check_id_sdk_ios/releases/download/1.0.1/SimpleSomething.xcframework.zip",
-            checksum: "43e2f815bc149409943a3911e93cfa6039e253fc37950e14b58db672662607f8")
+            name: "victoria_id_check_id_sdk_ios",
+            url: "https://api.github.com/repos/victoria-id/victoria_id_check_id_sdk_ios/releases/assets/185844235.zip",
+            checksum: "ab25f4b050ff3b51ef19dd401f130713964908acb4588890744fd3930ab7255a")
     ]
 )
